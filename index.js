46ded4929679793581884eda2e8e40d00bee9abf
Nicolas <ncardonab@unal.edu.co>
2020-06-02 20:20:05 -0500

    Mongoose Part 3

--- index.js
+++ index.js
@@ -11,14 +11,34 @@ connect.then((db) => {
   Dishes.create({
     name: "Uthappizza",
     description: "test",
-  })
+  }) // Returns a promise with the created dish
     .then((dish) => {
       console.log(dish);
 
-      return Dishes.find({});
+      return Dishes.findByIdAndUpdate(
+        dish._id,
+        {
+          $set: { description: "Updated test" },
+        },
+        {
+          // Once the updates of the dish is complete then this will return the updated dish back to us.
+          new: true,
+        }
+      ).exec();
     })
-    .then((dishes) => {
-      console.log(dishes);
+    .then((dish) => {
+      console.log(dish);
+
+      dish.comments.push({
+        rating: 5,
+        comment: "Please give me more",
+        author: "Leonardo Dicaprio",
+      });
+
+      return dish.save();
+    })
+    .then((dish) => {
+      console.log(dish);
 
       return Dishes.deleteOne({});
     })
